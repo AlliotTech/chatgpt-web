b45ea38655b5f90e4c6fde0c74f2ff49647ea0fc
Alliot <alliot@iots.vip>
2022-12-08 17:31:06 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
 import openai
-import json
-# import datetime
 from flask import Flask, request, render_template, redirect
 
 server = Flask(__name__)
@@ -9,21 +6,18 @@ server = Flask(__name__)
 openai.api_key = 'xxxxx'
 
 
-def get_res(input):
-    # print("开始处理: ", input)
+def get_completion(question):
     try:
-        # openai.log = True
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=f"{input}\n",
+            prompt=f"{question}\n",
             temperature=0.9,
             max_tokens=2048,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0.6,
-            stop=[" Human:", " AI:"]
+            stop=None
         )
-        # print(response)
     except Exception as e:
 
         print(e)
@@ -35,11 +29,14 @@ def get_res(input):
 def get_request_json():
     if request.method == 'POST':
         if len(request.form['question']) < 1:
-            return render_template('chat.html', question="null", res="问题不能为空")
+            return render_template(
+                'chat.html', question="null", res="问题不能为空")
         question = request.form['question']
         print("======================================")
         print("接到请求:", question)
-        res = get_res(question)
+        res = get_completion(question)
+        print("问题：\n", question)
+        print("答案：\n", res)
 
         return render_template('chat.html', question=question, res=str(res))
     return render_template('chat.html', question=0)
@@ -47,4 +44,3 @@ def get_request_json():
 
 if __name__ == '__main__':
     server.run(debug=True, host='0.0.0.0', port=80)
-
